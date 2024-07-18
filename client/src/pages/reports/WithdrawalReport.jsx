f54b0e5be5fb7d99c674f9fa166baa52e93947d4
Kenieboy <syntax0330@gmail.com>
2024-07-18 15:14:16 +0800

    7/18/24314

--- client/src/pages/reports/WithdrawalReport.jsx
+++ client/src/pages/reports/WithdrawalReport.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function WithdrawalReport() {
-  return <div>WithdrawalReport</div>;
+  const [startDate, setStartDate] = useState("2024-06-01");
+  const [endDate, setEndDate] = useState("2024-06-30");
+  return (
+    <div>
+      {" "}
+      <div className="flex gap-4 mt-6">
+        <label>
+          Start Date:
+          <input
+            className="w-full p-1 border border-gray-500 rounded-full mt-2"
+            type="date"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+          />
+        </label>
+        <label>
+          End Date:
+          <input
+            className="w-full p-1 border border-gray-500 rounded-full mt-2"
+            type="date"
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+          />
+        </label>
+      </div>
+    </div>
+  );
 }
 
 export default WithdrawalReport;
